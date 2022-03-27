0485198fd5e8821bb910662024e533549ac614e6
Tunisia Artope <artope@yahoo.com>
2022-03-27 12:31:43 -0400

    commit tree a80c2a155a07f873a55e42efa519b173e21065bb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do  
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    price = BakedGood.by_price
+    price.to_json  
+  end
+
+  get '/baked_goods/most_expensive' do 
+    most_expensive = BakedGood.by_price.first
+    most_expensive.to_json
+  end
 
 end
